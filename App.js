bbef98c8a1344b2ebbcbaa2ab041f06c6046d2a8
Nancy Onyejiaka <nancyonyejiaka@Nancys-MacBook-Pro.local>
2024-07-28 16:32:22 -0400

    finished up to 6.0

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import cors from 'cors';
 import express from 'express';
+import 'dotenv/config';
+import session from 'express-session';
 import mongoose from 'mongoose';
 import 'dotenv/config';
 import Lab5 from './Lab5/index.js';
 import CourseRoutes from './Kanbas/Courses/routes.js';
 import ModuleRoutes from './Kanbas/Modules/routes.js';
 import AssignmentRoutes from './Kanbas/Assignments/routes.js';
-import UserRoutes from './User/routes.js';
+import UserRoutes from './Users/routes.js';
 
 const CONNECTION_STRING =
   process.env.MONGO_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
-mongoose.connect(CONNECTION_STRING);
+mongoose
+  .connect(CONNECTION_STRING)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Failed to connect to MongoDB', err));
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.NETLIFY_URL || 'http://localhost:3000',
+  })
+);
+
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET || 'kanbas',
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: 'none',
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+
+app.use(session(sessionOptions));
+
 UserRoutes(app);
 
 app.get('/', (req, res) => {
